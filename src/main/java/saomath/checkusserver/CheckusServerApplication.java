97c18c5c6f04fc103c42363df67f41eda8984d97
Yunjeong <yunjeongiya@gmail.com>
2025-05-21 18:51:33 +0900

    feat: set up spring boot project

---
+++ src/main/java/saomath/checkusserver/CheckusServerApplication.java
@@ -0,0 +1,13 @@
+package saomath.checkusserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckusServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckusServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/saomath/checkusserver/CheckusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package saomath.checkusserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckusServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
