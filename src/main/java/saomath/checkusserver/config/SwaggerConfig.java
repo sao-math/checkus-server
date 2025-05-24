c5da6d9b5fb8e3e71f7aa6ac720da19a5cbac7b9
Yunjeong <yunjeongiya@gmail.com>
2025-05-24 22:49:06 +0900

    update: use Swagger instead of RestDocs for fast dev

---
+++ src/main/java/saomath/checkusserver/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package saomath.checkusserver.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CheckUS API 문서")
+                        .description("CheckUS 애플리케이션의 API 문서입니다.")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
