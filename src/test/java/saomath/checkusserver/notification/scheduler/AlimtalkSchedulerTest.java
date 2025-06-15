b59793c4c0d3c3dbebc525d7032ee65f5cdb2cdf
Yunjeong <yunjeongiya@gmail.com>
2025-06-15 19:54:24 +0900

    test: Add lenient strictness to AlimtalkSchedulerTest to resolve UnnecessaryStubbingException
    - Add @MockitoSettings(strictness = Strictness.LENIENT) annotation
    - Import required MockitoSettings and Strictness classes
    - Resolves unnecessary stubbing warnings in test execution

--- src/test/java/saomath/checkusserver/notification/scheduler/AlimtalkSchedulerTest.java
+++ src/test/java/saomath/checkusserver/notification/scheduler/AlimtalkSchedulerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import saomath.checkusserver.entity.AssignedStudyTime;
 import saomath.checkusserver.entity.ActualStudyTime;
 import saomath.checkusserver.notification.domain.AlimtalkTemplate;
@@ -23,6 +25,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class AlimtalkSchedulerTest {
     
     @Mock
